a69e548b8b0c346995859247fda83a027f700642
sumit <mazumdarsumit3@gmail.com>
2024-11-10 19:36:28 +0530

    add: updated the config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-'use client';
+"use client";
 import { useReadContract } from "thirdweb/react";
 import { client } from "./client";
-import { baseSepolia } from "thirdweb/chains";
+import { sepolia } from "thirdweb/chains";
 import { getContract } from "thirdweb";
 import { CampaignCard } from "@/components/CampaignCard";
 import { CROWDFUNDING_FACTORY } from "./constants/contracts";
@@ -10,24 +10,32 @@ export default function Home() {
   // Get CrowdfundingFactory contract
   const contract = getContract({
     client: client,
-    chain: baseSepolia,
+    chain: sepolia,
     address: CROWDFUNDING_FACTORY,
   });
 
   // Get all campaigns deployed with CrowdfundingFactory
-  const {data: campaigns, isLoading: isLoadingCampaigns, refetch: refetchCampaigns } = useReadContract({
+  const {
+    data: campaigns,
+    isLoading: isLoadingCampaigns,
+    refetch: refetchCampaigns,
+  } = useReadContract({
     contract: contract,
-    method: "function getAllCampaigns() view returns ((address campaignAddress, address owner, string name)[])",
-    params: []
+    method:
+      "function getAllCampaigns() view returns ((address campaignAddress, address owner, string name)[])",
+    params: [],
   });
+
+  console.log(campaigns);
 
   return (
     <main className="mx-auto max-w-7xl px-4 mt-4 sm:px-6 lg:px-8">
       <div className="py-10">
         <h1 className="text-4xl font-bold mb-4">Campaigns:</h1>
         <div className="grid grid-cols-3 gap-4">
-          {!isLoadingCampaigns && campaigns && (
-            campaigns.length > 0 ? (
+          {!isLoadingCampaigns &&
+            campaigns &&
+            (campaigns.length > 0 ? (
               campaigns.map((campaign) => (
                 <CampaignCard
                   key={campaign.campaignAddress}
@@ -36,8 +44,7 @@ export default function Home() {
               ))
             ) : (
               <p>No Campaigns</p>
-            )
-          )}
+            ))}
         </div>
       </div>
     </main>
